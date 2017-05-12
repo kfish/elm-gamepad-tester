c014a23ca34c3f1af930bb9bf246f137154522b8
Conrad Parker <conrad@metadecks.org>
2017-05-12 14:36:43 +0800

    Add publish-gh-pages.sh

---
+++ publish-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+REMOTE=kfish
+
+SOURCE=src/Main.elm
+TARGET=target/elm.js
+
+git push ${REMOTE} master
+if [ -e ${TARGET} ]; then
+	git add -f ${TARGET}
+	git commit -m 'Update ${TARGET}'
+	git push --force ${REMOTE} HEAD:gh-pages
+	git reset --hard HEAD^
+else
+	echo "Please build ${TARGET} with:"
+        echo
+	echo "    elm-make ${SOURCE} --output ${TARGET}"
+        echo
+fi
